530d75b6e361a0b115dd37f8cb11210d6300bc5b
Ksenia Skarzhynets <109911970+csnotgo@users.noreply.github.com>
2023-05-20 23:46:52 +0300

    add adaptive styles

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 const getElem = (elem) => document.querySelector(elem);
+const getAllElems = (elem) => document.querySelectorAll(elem);
 
 const arrow = getElem(".arrow");
 const moreInfo = getElem(".more-info");
+const infoBoxMob = getElem(".info-box-mob");
 const infoBox = getElem(".info-box");
 const list = getElem(".list");
+const signUpBtns = getAllElems(".sign-up-btn");
+const form = getElem(".form");
 
 moreInfo.addEventListener("click", showInfo);
+signUpBtns.forEach((elem) => elem.addEventListener("click", onClick));
+window.addEventListener("resize", debounce(createMarkup, 250));
 
 async function getAll() {
   try {
@@ -17,50 +23,94 @@ async function getAll() {
   }
 }
 
+async function createMarkup() {
+  const products = await getAll();
+  const productImage = products[0]?.images.map(({ src }) => src);
+  const arr = [];
+
+  const width = window.screen.width;
+  let productCount = 0;
+
+  if (width <= 923) {
+    productCount = 6;
+  } else if (width >= 924 && width <= 1023) {
+    productCount = 9;
+  } else if (width >= 1024) {
+    productCount = 12;
+  }
+
+  products?.map(({ variants }) => {
+    arr.push(variants);
+  });
+
+  const items = arr.flatMap((item) => item).slice(0, productCount);
+
+  const listItems = items.map(({ title, price }) => markup(productImage, title, price)).join(" ");
+
+  list.innerHTML = listItems;
+
+  const orderBtn = getAllElems(".order-btn");
+  orderBtn.forEach((elem) => elem.addEventListener("click", onClick));
+}
+
+function markup(url, title, price) {
+  return `<li class="max-w-[342px] sm:w-[342px] h-[402px] relative md:max-w-[300px]">
+        <span class="bg-black p-2 flex justify-center items-center w-[47px] h-[24px] rounded absolute top-3 left-3 ">
+          <p class="text-lightSand text-xs ">USED</p>
+        </span>
+        <img src="${url}" alt="${title}" class="w-full h-[300px] border  border-black rounded">
+        <div class="flex justify-between py-3">
+          <div class="font-bold text-sm">
+            <p>${title}</p>
+            <p>${price} $</p>
+          </div>
+          <div class="text-right text-sm">
+            <p class="font-medium">Condition</p>
+            <p>Slightly used</p>
+          </div>
+        </div>
+        <button type="button" class="order-btn bg-black text-white text-sm font-bold py-4 rounded w-full h-[42px] flex items-center justify-center hover:bg-orangeHover focus:bg-orangeHover">
+        PICK-UP IN <span class="underline ml-1">2200</span>
+        </button>
+    </li>`;
+}
+
 function showInfo() {
   infoBox.classList.toggle("hidden");
+  const width = window.screen.width;
+
+  if (width >= 320 && width <= 360) {
+    moreInfo.style.height = "500px";
+  } else if (width >= 361 && width <= 704) {
+    moreInfo.style.height = "485px";
+  } else if (width >= 705 && width <= 921) {
+    moreInfo.style.height = "435px";
+  } else if (width >= 922) {
+    moreInfo.style.height = "300px";
+  }
   arrow.style.transform = "rotate(180deg)";
 
   if (infoBox.classList.contains("hidden")) {
     arrow.style.transform = "rotate(0)";
+    moreInfo.style.height = "auto";
   }
 }
 
-async function createMarkup() {
-  const products = await getAll();
-  console.log(products);
-  const productImage = products[0]?.images.map(({ src }) => src);
+function onClick(e) {
+  e.preventDefault();
+  alert("More in next updates (˵´•‿•`˵ ⑅)");
+  e.target.blur();
+  form.reset();
+}
 
-  const markup = products
-    ?.map(({ variants }) => {
-      console.log(variants);
-      return variants
-        .map(({ title, price }) => {
-          return `<li class="max-w-[342px] h-[402px] relative">
-          <span class="bg-black p-2 flex justify-center items-center w-[47px] h-[24px] rounded absolute top-3 left-3 ">
-            <p class="text-lightSand text-xs ">USED</p>
-          </span>
-          <img src="${productImage}" alt="${title}" class="w-full h-[300px] border  border-black rounded">
-          <div class="flex justify-between py-3">
-            <div class="font-bold text-sm">
-              <p>${title}</p>
-              <p>${price} $</p>
-            </div>
-            <div class="text-right text-sm">
-              <p class="font-medium">Condition</p>
-              <p>Slightly used</p>
-            </div>
-          </div>
-          <button type="button" class="bg-black text-white text-sm font-bold py-4 rounded w-full h-[42px] flex items-center justify-center hover:bg-orangeHover focus:bg-orangeHover">
-          PICK-UP IN <span class="underline ml-1">2200</span>
-          </button>
-      </li>`;
-        })
-        .join(" ");
-    })
-    .join(" ");
-
-  list.innerHTML = markup;
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
 }
 
 createMarkup();
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        mono: ["monospace", "Space Mono"],
+      },
       fontSize: {
         xs: ["12px", "15px"],
         sm: ["14px", "18px"],
         smFooter: ["14px", "21px"],
         base: ["16px", "20px"],
+        xl: ["20px", "26px"],
       },
       colors: {
         lightSand: "#FCF7E6",
@@ -16,6 +20,28 @@ module.exports = {
       },
       boxShadow: {
         outline: "0px 0px 10px 4px rgba(237,94,33,0.5)",
+      },
+      screens: {
+        sm: "400px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+      },
+      container: {
+        center: true,
+        sm: "max-width: 400px",
+        md: "max-width: 768px",
+        lg: "max-width: 1024px",
+        xl: "max-width: 1280px",
+        "2xl": "max-width: 1280px",
+        padding: {
+          DEFAULT: "24px",
+          sm: "24px",
+          lg: "0",
+          xl: "0",
+          "2xl": "0",
+        },
       },
     },
   },
